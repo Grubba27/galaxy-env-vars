e8759424f7349495dcfc6f7f36c28b3a88131e8a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-09-29 11:32:11 -0300

    first commit

---
+++ server/main.js
@@ -0,0 +1,22 @@
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
+
+async function insertLink({ title, url }) {
+  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
+}
+
+Meteor.startup(async () => {
+  console.log("Using process env", process.env["galaxy.meteor.com"]); //
+  console.log("Using process env", process.env["galaxy.meteor.com"]); //
+  console.log(
+    "Using Meteor settings galaxy",
+    Meteor.settings["galaxy.meteor.com"]
+  );
+  console.log("Using Meteor settings", Meteor.settings);
+});
+
+Meteor.startup(function () {
+  if (Meteor.settings)
+    for (let variableName in Meteor.settings)
+      process.env[variableName] = Meteor.settings[variableName];
+});
